70f6a12d4543fa1c45ecb954101f832d4433ce8f
laurapadilla <hi@laurapadilla.xyz>
2022-09-07 11:11:30 -0400

    Fix styles and add some basic logic

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -14,9 +14,9 @@ class ReviewsController < ApplicationController
 
   def create
     # take info from form and add it to database
-    @review = Review.new(params.require(:review).permit(:title, :body, :score))
+    @review = Review.new(form_params)
 
-    # save the reviews
+    # save this to the database
     @review.save
 
     # redirect back to home page
@@ -24,7 +24,39 @@ class ReviewsController < ApplicationController
   end
 
   def show
-    # individual review page
+    # show the individual review page
     @review = Review.find(params[:id])
+  end
+
+  # delete the review
+  def destroy
+    # find the individual review
+    @review = Review.find(params[:id])
+
+    # delete/destroy review
+    @review.destroy
+
+    # redirect back to homepage
+    redirect_to root_path, status: :see_other
+  end
+
+  def edit
+    # find the individual review to edit
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    # find the individual review to update
+    @review = Review.find(params[:id])
+
+    # update with the new info from form
+    @review.update(form_params)
+
+    # redirect back to review
+    redirect_to review_path, status: :see_other
+  end
+
+  def form_params
+    params.require(:review).permit(:title, :body, :score)
   end
 end
