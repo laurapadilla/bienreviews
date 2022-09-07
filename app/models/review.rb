06353015ba15b6490165f7f58893d2599e5356d8
laurapadilla <hi@laurapadilla.xyz>
2022-09-07 11:56:32 -0400

    add form validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,9 @@
+# frozen_string_literal: true
+
+# this is my model for the reviews
+
 class Review < ApplicationRecord
+  validates :title, presence: true
+  validates :body, length: { minimum: 10 }, presence: true
+  validates :score, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }, presence: true
 end
